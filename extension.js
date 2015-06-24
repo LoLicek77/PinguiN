28a4e1c6daff1e30b0f33792af9cef98247482ad
LoLicek77 <romikor1999@gmail.com>
2015-06-24 21:48:25 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -40,7 +40,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me I :blue_heart: Bonki");
+                    API.sendChat("I :blue_heart: @βσηҟɨ");
                 }
             }
         };
